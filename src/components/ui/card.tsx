907610a659ebfd05d3bf63dc3faca8dc0659dd1a
Kishore S <ks7371083@gmail.com>
2026-01-27 06:43:56 +0000

    all ok the ui design and some other design is not good but  i need backg

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-sm transition-all duration-300 ease-in-out",
+      "rounded-lg border bg-card/75 backdrop-blur-sm text-card-foreground shadow-sm transition-all duration-300 ease-in-out",
       className
     )}
     {...props}
